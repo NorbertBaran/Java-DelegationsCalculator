5568ec829eb801f9ca17ed18e54ee2ab1ee6e976
asokolowski <areksokolowski@gmail.com>
2020-01-09 22:55:08 +0100

    Initial commit

---
+++ src/main/java/uj/java/pwj2019/delegations/Calc.java
@@ -0,0 +1,12 @@
+package uj.java.pwj2019.delegations;
+
+import java.math.BigDecimal;
+
+public class Calc {
+
+    BigDecimal calculate(String start, String end, BigDecimal dailyRate) throws IllegalArgumentException {
+        //TODO implement
+        return null;
+    }
+
+}
